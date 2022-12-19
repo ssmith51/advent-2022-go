641612dc30f48f4cf2e2d15a840c1c5cd77b086e
Spencer Smith <ssmith2@netjets.com>
2022-12-19 16:03:54 -0500

    Day 06 Complete

--- advent-06/main.go
+++ advent-06/main.go
@@ -1,7 +1,94 @@
 package main
 
-import "log"
+import (
+	"advent-06/internal/pgk/utils"
+	"bufio"
+	"log"
+	"os"
+)
+
+func load(filename string) []rune {
+
+	fi, err := os.Open(filename)
+	utils.HandleError(err)
+	defer fi.Close()
+
+	scanner := bufio.NewScanner(fi)
+	runes := []rune{}
+	for scanner.Scan() {
+		line := scanner.Text()
+		runes = []rune(line)
+		// log.Println(runes)
+
+	}
+
+	return runes
+
+}
+
+func puzzle_1(markers []rune) {
+
+	packets := []rune{}
+	processed := 0
+	start := false
+	msgLen := 4
+
+	for processed < len(markers) && !start {
+		// log.Println("Round: ", processed, " signal: ", string(markers[processed]))
+
+		packets = append(packets, markers[processed])
+
+		if len(packets) > msgLen {
+			packets = packets[1:]
+			startPacket := make(map[rune]string)
+			for _, p := range packets {
+				startPacket[p] = string(p)
+			}
+
+			if len(startPacket) == msgLen {
+				start = true
+			}
+		}
+		processed++
+	}
+
+	log.Printf("Start Packet Discovered after %d signals", processed)
+
+}
+
+func puzzle_2(markers []rune) {
+
+	packets := []rune{}
+	processed := 0
+	start := false
+	msgLen := 14
+
+	for processed < len(markers) && !start {
+		// log.Println("Round: ", processed, " signal: ", string(markers[processed]))
+
+		packets = append(packets, markers[processed])
+
+		if len(packets) > msgLen {
+			packets = packets[1:]
+			startPacket := make(map[rune]string)
+			for _, p := range packets {
+				startPacket[p] = string(p)
+			}
+
+			if len(startPacket) == msgLen {
+				start = true
+			}
+		}
+		processed++
+	}
+
+	log.Printf("Start Packet Discovered after %d signals", processed)
+
+}
 
 func main() {
 	log.Println("Advent of Code Day 06")
+	markers := load("input.txt")
+	puzzle_1(markers)
+	puzzle_2(markers)
 }
